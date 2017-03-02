4f69fc72684713bfcf1922302d78f65f7bf400f8
Francesco De Simone <francesco.desimone@injenia.it>
2017-03-02 15:50:10 +0100

    fix

--- datalab/instance_details.sh
+++ datalab/instance_details.sh
@@ -2,6 +2,6 @@
 
 # Remove "_" from USER because "_" can not be used for instance name
 export INSTANCE_NAME=datalabvm-${USER//_/}
-export ZONE=europe-west1-ab
+export ZONE=europe-west1-b
 export MACHINE_TYPE=n1-standard-1
 
